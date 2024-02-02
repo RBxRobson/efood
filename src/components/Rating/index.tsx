90edae5127c3d181b3576ebc511eb312b95811d5
Robson.B <robsonnbarbosa0@gmail.com>
2024-02-02 14:16:55 -0300

    feat - criando o componente para avaliação

---
+++ src/components/Rating/index.tsx
@@ -0,0 +1,17 @@
+import { RatingContainer } from './styles'
+import star from '../../assets/images/estrela.svg'
+
+export type Props = {
+  children: string
+}
+
+const Rating = ({ children }: Props) => {
+  return (
+    <RatingContainer>
+      {children}
+      <img src={star} alt="estrela" />
+    </RatingContainer>
+  )
+}
+
+export default Rating
---
+++ src/components/Rating/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const RatingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 18px;
+  font-weight: bold;
+`
