c0c18f18317f3efc948e2fbf9554d90b16fefa30
Robson.B <robsonnbarbosa0@gmail.com>
2024-02-05 13:36:52 -0300

    feat - adicionando model card

---
+++ src/models/Card.ts
@@ -0,0 +1,26 @@
+class Game {
+  infos: string[]
+  description: string
+  rating?: string
+  image: string
+  title: string
+  id: number
+
+  constructor(
+    infos: string[],
+    description: string,
+    rating: string,
+    image: string,
+    title: string,
+    id: number
+  ) {
+    this.infos = infos
+    this.description = description
+    this.rating = rating
+    this.image = image
+    this.title = title
+    this.id = id
+  }
+}
+
+export default Game
