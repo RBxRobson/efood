375d911abb4c066143c4d94cc48d3d36eba37493
Robson.B <robsonnbarbosa0@gmail.com>
2024-02-11 10:07:40 -0300

    feat - adicionando as rotas

---
+++ src/pages/Restaurant/index.tsx
@@ -0,0 +1,57 @@
+import Banner from '../../components/Banner'
+import CardList from '../../components/CardList'
+import Header from '../../components/Header'
+import Card from '../../models/Card'
+
+import restaurante from '../../assets/images/hioki_sushi.png'
+
+const cards: Card[] = [
+  {
+    id: 1,
+    description:
+      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
+    rating: '4.8',
+    title: 'Residente Evil 4',
+    infos: ['Destaque do Dia', 'Italiana'],
+    image: restaurante
+  },
+  {
+    id: 2,
+    description:
+      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
+    rating: '4.8',
+    title: 'Residente Evil 4',
+    infos: ['Italiana'],
+    image: restaurante
+  },
+  {
+    id: 3,
+    description:
+      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
+    rating: '4.8',
+    title: 'Residente Evil 4',
+    infos: ['Italiana'],
+    image: restaurante
+  },
+  {
+    id: 4,
+    description:
+      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
+    rating: '4.8',
+    title: 'Residente Evil 4',
+    infos: ['Italiana'],
+    image: restaurante
+  }
+]
+
+const Restaurant = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <CardList columns={3} cards={cards} />
+    </>
+  )
+}
+
+export default Restaurant
