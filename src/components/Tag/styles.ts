c06fd5ad145a9be75551f7f710e594255c3e4451
Robson.B <robsonnbarbosa0@gmail.com>
2024-02-14 17:41:24 -0300

    chore - ajuste no padding das tags

--- src/components/Tag/styles.ts
+++ src/components/Tag/styles.ts
@@ -6,7 +6,7 @@ import { Props } from '.'
 //* Usando a props passada no componente
 export const TagContainer = styled.div<Props>`
   display: inline-block;
-  padding: 6px 4px;
+  padding: 6px 8px;
   font-size: ${(props) => (props.size === 'big' ? '14px' : '12px')};
   font-weight: bold;
   background-color: ${colors.txt_home_color};
