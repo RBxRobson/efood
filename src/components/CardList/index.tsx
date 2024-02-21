607c9eb45bb7019904c006e29dc23e699098562a
Robson.B <robsonnbarbosa0@gmail.com>
2024-02-21 16:59:44 -0300

    feat - integrando api na listagem de restaurantes

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,42 +1,42 @@
-import Restaurant from '../../models/Restaurant'
-import Product from '../../models/Product'
+import { Restaurant } from '../../types/restaurant'
 import RestaurantCard from '../RestaurantCard'
-import { List } from './styles'
 import ProductCard from '../ProductCard'
 
+import { List } from './styles'
+
 export type Props = {
-  columns: 3 | 2
-  cards: (Restaurant | Product)[]
+  list: 'restaurant' | 'menu'
+  restaurants: Restaurant[]
 }
 
-const CardList = ({ cards, columns }: Props) => {
+const CardList = ({ list, restaurants }: Props) => {
+  //* Array para armazenar os restaurantes em ordem de prioridade a partir do destaque
+  const sortedRestaurants = [...restaurants].sort((restaurant) =>
+    restaurant.destacado ? -1 : 1
+  )
+
   return (
     <div className="container">
-      <List columns={columns}>
-        {cards.map((card) => {
-          if ('infos' in card) {
-            const restaurant = card as Restaurant
-            return (
-              <RestaurantCard
-                key={restaurant.id}
-                description={restaurant.description}
-                image={restaurant.image}
-                infos={restaurant.infos}
-                title={restaurant.title}
-                rating={restaurant.rating}
-              />
-            )
-          } else {
-            const product = card as Product
-            return (
-              <ProductCard
-                key={product.id}
-                description={product.description}
-                image={product.image}
-                title={product.title}
-              />
-            )
-          }
+      <List list={list}>
+        {sortedRestaurants.map((restaurant) => {
+          return list === 'restaurant' ? (
+            <RestaurantCard
+              key={restaurant.id}
+              description={restaurant.descricao}
+              image={restaurant.capa}
+              type={restaurant.tipo}
+              title={restaurant.titulo}
+              rating={restaurant.avaliacao}
+              featured={restaurant.destacado}
+            />
+          ) : (
+            <ProductCard
+              key={restaurant.cardapio.id}
+              description={restaurant.cardapio.descricao}
+              image={restaurant.cardapio.foto}
+              name={restaurant.cardapio.nome}
+            />
+          )
         })}
       </List>
     </div>
--- src/components/CardList/styles.ts
+++ src/components/CardList/styles.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components'
 
 import { Props } from '.'
 
-export const List = styled.ul<Omit<Props, 'cards'>>`
+export const List = styled.ul<Omit<Props, 'restaurants'>>`
   display: grid;
   grid-template-columns: ${(props) =>
-    props.columns === 3 ? '1fr 1fr 1fr' : '1fr 1fr'};
-  column-gap: ${(props) => (props.columns === 3 ? '32px' : '80px')};
-  row-gap: ${(props) => (props.columns === 3 ? '32px' : '48px')};
-  margin-top: ${(props) => (props.columns === 3 ? '56px' : '80px')};
+    props.list === 'menu' ? '1fr 1fr 1fr' : '1fr 1fr'};
+  column-gap: ${(props) => (props.list === 'menu' ? '32px' : '80px')};
+  row-gap: ${(props) => (props.list === 'menu' ? '32px' : '48px')};
+  margin-top: ${(props) => (props.list === 'menu' ? '56px' : '80px')};
   margin-bottom: 120px;
 `
