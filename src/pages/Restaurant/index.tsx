9bfc67dd0894fe9d846075d8951b3596c5cad12b
Robson.B <robsonnbarbosa0@gmail.com>
2024-03-02 13:18:44 -0300

    feat - integrando api e redux

--- src/pages/Restaurant/index.tsx
+++ src/pages/Restaurant/index.tsx
@@ -1,41 +1,19 @@
 import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
 
 import Banner from '../../components/Banner'
 import CardList from '../../components/CardList'
 import Header from '../../components/Header'
-import { Menu, Restaurant as RestaurantType } from '../../types/restaurant'
 import ProductCard from '../../components/ProductCard'
 import Modal from '../../components/Modal'
 
-interface ModalState extends Omit<Menu, 'id'> {
-  isVisible: boolean
-}
+import { useGetRestaurantQuery } from '../../services/api'
 
 const Restaurant = () => {
   const { id } = useParams()
-  const [restaurant, setRestaurant] = useState<RestaurantType>()
-  const [modal, setModal] = useState<ModalState>({
-    isVisible: false,
-    foto: '',
-    nome: '',
-    descricao: '',
-    porcao: '',
-    preco: 0
-  })
-
-  useEffect(() => {
-    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`)
-      .then((res) => res.json())
-      .then((restaurant) => setRestaurant(restaurant))
-  }, [id])
+  const { data: restaurant } = useGetRestaurantQuery(id!)
 
   if (!restaurant) {
     return <h3>Carregando...</h3>
-  }
-
-  const closeModal = () => {
-    setModal({ ...modal, isVisible: false })
   }
 
   return (
@@ -49,34 +27,11 @@ const Restaurant = () => {
       <CardList list="menu">
         <>
           {restaurant.cardapio.map((menu) => (
-            <ProductCard
-              key={menu.id}
-              description={menu.descricao}
-              name={menu.nome}
-              image={menu.foto}
-              onClick={() =>
-                setModal({
-                  isVisible: true,
-                  foto: menu.foto,
-                  nome: menu.nome,
-                  descricao: menu.descricao,
-                  porcao: menu.porcao,
-                  preco: menu.preco
-                })
-              }
-            />
+            <ProductCard key={menu.id} menu={menu} />
           ))}
         </>
       </CardList>
-      <Modal
-        isVisible={modal.isVisible}
-        image={modal.foto}
-        name={modal.nome}
-        description={modal.descricao}
-        portion={modal.porcao}
-        price={modal.preco}
-        closeModal={closeModal}
-      />
+      <Modal />
     </>
   )
 }
