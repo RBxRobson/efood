759c67807c34df8518297e69e981b5605d943d13
Robson.B <robsonnbarbosa0@gmail.com>
2024-03-02 13:18:15 -0300

    feat - integrando api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useGetRestaurantsQuery } from '../../services/api'
+
 import CardList from '../../components/CardList'
 import Header from '../../components/Header'
-import { Restaurant } from '../../types/restaurant'
 import RestaurantCard from '../../components/RestaurantCard'
 
 const Home = () => {
-  const [restaurants, setRestaurants] = useState<Restaurant[]>([])
-
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/efood/restaurantes')
-      .then((res) => res.json())
-      .then((res) => setRestaurants(res))
-  }, [])
+  const { data: restaurants } = useGetRestaurantsQuery()
 
   //* Array para armazenar os restaurantes em ordem de prioridade a partir do destaque
-  const sortedRestaurants = [...restaurants].sort((restaurant) =>
-    restaurant.destacado ? -1 : 1
-  )
+  const sortedRestaurants = restaurants
+    ? [...restaurants!].sort((restaurant) => (restaurant.destacado ? -1 : 1))
+    : []
 
   return (
     <>
