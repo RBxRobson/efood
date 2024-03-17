c3ad4e12862bc8da4c1418e793ccfe1e48ecbcd8
Robson.B <robsonnbarbosa0@gmail.com>
2024-03-17 11:03:48 -0300

    feat - criando a tipagem dos dados do checkout

---
+++ src/types/payload.ts
@@ -0,0 +1,29 @@
+type Product = {
+  id: number
+  price: number
+}
+
+export type PurchasePayload = {
+  product: Product[]
+  delivery: {
+    receiver: string
+    address: {
+      description: string
+      city: string
+      zipCode: string
+      number: number
+      complement?: string
+    }
+  }
+  payment: {
+    card: {
+      name: string
+      number: string
+      code: number
+      expires: {
+        month: number
+        year: number
+      }
+    }
+  }
+}
