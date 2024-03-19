29e4f69e58c61630fb1b8b7b72fd8b61a9ffd09e
RBxRobson <robsonnbarbosa0@gmail.com>
2024-03-19 15:28:42 -0300

    feat - integrando reducer form a store

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import cartReducer from './reducers/cart'
 import modalReducer from './reducers/modal'
+import formReducer from './reducers/form'
 import api from '../services/api'
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
     cart: cartReducer,
+    form: formReducer,
     [api.reducerPath]: api.reducer
   },
   middleware: (getDefaultMiddleware) =>
