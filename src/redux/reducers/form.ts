633a2d4273bcbb27301003fb118d99304c89d647
RBxRobson <robsonnbarbosa0@gmail.com>
2024-03-21 17:30:08 -0300

    chore - ajustando lógica do reducer form

--- src/redux/reducers/form.ts
+++ src/redux/reducers/form.ts
@@ -1,7 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-const initialState: PurchasePayload = {
-  product: [],
+const initialState: Omit<PurchasePayload, 'products' | 'payment'> = {
   delivery: {
     receiver: '',
     address: {
@@ -11,75 +10,33 @@ const initialState: PurchasePayload = {
       number: Number(''),
       complement: ''
     }
-  },
-  payment: {
-    card: {
-      name: '',
-      number: '',
-      code: Number(''),
-      expires: {
-        month: Number(''),
-        year: Number('')
-      }
-    }
   }
-}
-
-type DeliveryInfos = {
-  receiver: string
-  address: string
-  city: string
-  cep: string
-  number: string
-  complement?: string
-}
-
-type PaymentInfos = {
-  cardName: string
-  cardNumber: string
-  cvv: string
-  expiredMonth: string
-  expiredYear: string
-  products: Product[]
 }
 
 const formSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    formDelivery: (state, action: PayloadAction<DeliveryInfos>) => {
-      const { receiver, address, city, cep, number, complement } =
-        action.payload
-
+    formDelivery: (
+      state,
+      action: PayloadAction<Omit<PurchasePayload, 'products' | 'payment'>>
+    ) => {
+      const { receiver } = action.payload.delivery
+      const { city, description, number, zipCode, complement } =
+        action.payload.delivery.address
       state.delivery = {
         receiver,
         address: {
-          description: address,
           city,
-          zipCode: cep,
-          number: Number(number),
+          description,
+          number,
+          zipCode,
           complement
         }
       }
-    },
-    formPayment: (state, action: PayloadAction<PaymentInfos>) => {
-      const { cardName, cardNumber, cvv, expiredMonth, expiredYear, products } =
-        action.payload
-
-      state.payment.card = {
-        name: cardName,
-        number: cardNumber,
-        code: Number(cvv),
-        expires: {
-          month: Number(expiredMonth),
-          year: Number(expiredYear)
-        }
-      }
-
-      state.product = products
     }
   }
 })
 
-export const { formDelivery, formPayment } = formSlice.actions
+export const { formDelivery } = formSlice.actions
 export default formSlice.reducer
